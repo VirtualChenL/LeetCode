ea72d9d3666fa147ae1fb39a710694234147ab95
Virtual.chen <928247556@qq.com>
2018-11-27 19:22:01 +0800

    MiddleoftheLinkedList

---
+++ src/MiddleoftheLinkedList.java
@@ -0,0 +1,41 @@
+/**
+ * 链表的中间节点
+ * @author Administrator
+ *
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class MiddleoftheLinkedList {
+	 public ListNode middleNode(ListNode head) {
+	        int len=0;
+	        ListNode p=head;
+	        ListNode q=head;
+	        while(p!=null)
+	        {
+	            len++;
+	            p=p.next;
+	        }
+	        if(len==1)
+	            return head;
+	        else{
+	        if(len%2==1)
+	        { 
+	            for(int i=0;i<Math.ceil(len/2);i++)
+	                q=q.next;
+	            return q;
+	        }
+	        else
+	        {
+	            for(int i=0;i<len/2;i++)
+	            q=q.next;
+	            return q;
+	        }
+	        }
+	    }
+}
