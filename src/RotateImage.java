3a2389feafe446076f59f50f11cb84df74636ddd
Virtual.chen <928247556@qq.com>
2018-11-30 15:12:43 +0800

    RotateImage

---
+++ src/RotateImage.java
@@ -0,0 +1,22 @@
+/**
+ * 旋转图像
+ * @author VirtualChen 
+ *
+ */
+public class RotateImage {
+	public void rotate(int[][] matrix) {
+		int n=matrix.length;
+		for(int i=0;i<n/2;i++)
+		{
+			for(int j=i;j<n-1-i;j++)
+			{
+			int m=matrix[i][j];
+			matrix[i][j]=matrix[n-1-j][i];
+			matrix[n-1-j][i]=matrix[n-1-i][n-1-j];
+			matrix[n-1-i][n-1-j]=matrix[j][n-1-i];
+			matrix[j][n-1-i]=m;
+			}
+		}
+		
+    }
+}
