662c539a7f81b312a03efc971a2da34b255b0afc
Virtual.chen <928247556@qq.com>
2018-12-25 09:19:33 +0800

    AddBinary

---
+++ src/AddBinary.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+/**
+ * FileName: AddBinary
+ * Description: 二进制求和
+ * Author:   @VirtualChen
+ * Date:     2018/12/24 0024 下午 8:42
+ */
+
+public class AddBinary {
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1, j = b.length() -1, carry = 0;
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        if (carry != 0) sb.append(carry);
+        return sb.reverse().toString();
+    }
+}
