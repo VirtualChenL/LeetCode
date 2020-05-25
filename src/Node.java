460f0177f80bb6c6f3f15d722796ad5b9c53dcb3
cleverrchen77 <928247556@qq.com>
2020-05-25 16:44:44 +0800

    Update Node.java

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,5 @@
+package src;
+
 import java.util.List;
 
 /**
@@ -16,5 +18,17 @@ public class Node {
     public Node(int _val,List<Node> _children) {
         val = _val;
         children = _children;
+    }
+
+    //双向链表
+    public Node left;
+    public Node right;
+    public Node(int _val) {
+        val = _val;
+    }
+    public Node(int _val,Node _left,Node _right) {
+        val = _val;
+        left = _left;
+        right = _right;
     }
 }
