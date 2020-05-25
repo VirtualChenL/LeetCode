2ec392999b81f56a981ba939687d7c03a9b53c7f
cleverrchen77 <928247556@qq.com>
2020-05-25 16:45:59 +0800

    MaxQueue

---
+++ src/Themaximumvalueofthequeue.java
@@ -0,0 +1,41 @@
+/**
+ * FileName: Themaximumvalueofthequeue
+ * Description: 队列的最大值
+ * 请定义一个队列并实现函数 max_value 得到队列里的最大值，要求函数max_value、push_back 和 pop_front 的均摊时间复杂度都是O(1)
+ * Date:     2020/4/30 17:29
+ */
+package src;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MaxQueue {
+    Queue<Integer> que;
+    Deque<Integer> deq;
+
+    public MaxQueue() {
+        que = new LinkedList<>();  //队列：插入和删除
+        deq = new LinkedList<>();  //双端队列：获取最大值
+    }
+
+    public int max_value() {
+        return deq.size()>0?deq.peek():-1;  //双端队列的队首为que的最大值
+    }
+
+    public void push_back(int value) {
+        que.offer(value);  //value入队
+        while(deq.size()>0 && deq.peekLast()<value){
+            deq.pollLast();  //将deq队尾小于value的元素删掉
+        }
+        deq.offerLast(value);  //将value放在deq队尾
+    }
+
+    public int pop_front() {
+        int tmp = que.size()>0?que.poll():-1;  //获得队首元素
+        if(deq.size()>0 && deq.peek().equals(tmp)){
+            deq.poll();  //如果出队的元素是当前最大值，将deq的队首出队
+        }
+        return tmp;
+    }
+}
