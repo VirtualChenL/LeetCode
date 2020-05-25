42af889da2852e8fc86848cdf3e0d2262fb45ffd
cleverrchen77 <928247556@qq.com>
2020-05-25 16:43:37 +0800

    Update BalancedBinaryTree.java

--- src/BalancedBinaryTree.java
+++ src/BalancedBinaryTree.java
@@ -1,3 +1,5 @@
+package src;
+
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -34,4 +36,24 @@ public class BalancedBinaryTree {
         }
         return Integer.max(TreeHeight(p.right), TreeHeight(p.left)) + 1;
     }
+
+
+    /**
+     * 后序遍历+剪枝
+     * @param root
+     * @return
+     */
+    public boolean isBalanced1(TreeNode root) {
+        return recur(root) != -1;
+    }
+
+    private int recur(TreeNode root) {
+        if (root == null) return 0;
+        int left = recur(root.left);
+        if(left == -1) return -1;
+        int right = recur(root.right);
+        if(right == -1) return -1;
+        return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
+    }
+
 }
