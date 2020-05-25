d301d9584ebc7aa32ff576897586429f39690642
cleverrchen77 <928247556@qq.com>
2020-05-25 16:45:11 +0800

    Translatenumbersintotrings

---
+++ src/Translatenumbersintotrings.java
@@ -0,0 +1,47 @@
+/**
+ * FileName: Translatenumbersintotrings
+ * Description: 把数字翻译成字符串
+ * 给定一个数字，我们按照如下规则把它翻译为字符串：0 翻译成 “a” ，1 翻译成 “b”，……，
+ * 11 翻译成 “l”，……，25 翻译成 “z”。一个数字可能有多个翻译。请编程实现一个函数，用来计算一个数字有多少种不同的翻译方法
+ * Author:   @VirtualChen
+ * Date:     2020/4/24 15:11
+ */
+package src;
+
+public class Translatenumbersintotrings {
+    /**
+     * 动态规划
+     * @param num
+     * @return
+     */
+    public int translateNum(int num) {
+        String s = String.valueOf(num);
+        int a = 1, b = 1;
+        for(int i = 2; i <= s.length(); i++) {
+            String tmp = s.substring(i - 2, i);
+            int c = tmp.compareTo("10") >= 0 && tmp.compareTo("25") <= 0 ? a + b : a;
+            b = a;
+            a = c;
+        }
+        return a;
+    }
+
+    /**
+     * 数字求余
+     * @param num
+     * @return
+     */
+    public int translateNum1(int num) {
+        int a = 1, b = 1, x, y = num % 10;
+        while(num != 0) {
+            num /= 10;
+            x = num % 10;
+            int tmp = 10 * x + y;
+            int c = (tmp >= 10 && tmp <= 25) ? a + b : a;
+            b = a;
+            a = c;
+            y = x;
+        }
+        return a;
+    }
+}
