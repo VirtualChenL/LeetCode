cb58aeab08a7d8324e8dede5800c8e1abfb50c97
cleverrchen77 <928247556@qq.com>
2020-05-26 13:16:50 +0800

    Thelastnumberleftinthecircle

---
+++ src/Thelastnumberleftinthecircle.java
@@ -0,0 +1,36 @@
+/**
+ * FileName: Thelastnumberleftinthecircle
+ * Description: 圆圈中最后剩下的数字
+ * 0,1,,n-1这n个数字排成一个圆圈，从数字0开始，每次从这个圆圈里删除第m个数字。
+ * 求出这个圆圈里剩下的最后一个数字。
+ * 例如，0、1、2、3、4这5个数字组成一个圆圈，从数字0开始每次删除第3个数字，
+ * 则删除的前4个数字依次是2、0、4、1，因此最后剩下的数字是3
+ * Author:   @VirtualChen
+ * Date:     2020/3/30 20:49
+ */
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Thelastnumberleftinthecircle {
+    /**
+     * 模拟算法
+     * @param n
+     * @param m
+     * @return
+     */
+    public int lastRemaining(int n, int m) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(i);
+        }
+        int idx = 0;
+        while (n > 1) {
+            idx = (idx + m - 1) % n;
+            list.remove(idx);
+            n--;
+        }
+        return list.get(0);
+    }
+}
