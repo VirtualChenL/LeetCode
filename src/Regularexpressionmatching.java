431cf63687331e9ed15c8857ff63762f98042875
cleverrchen77 <928247556@qq.com>
2020-05-26 13:18:16 +0800

    Regularexpressionmatching

---
+++ src/Regularexpressionmatching.java
@@ -0,0 +1,45 @@
+/**
+ * FileName: Regularexpressionmatching
+ * Description:正则表达式匹配
+ * 请实现一个函数用来匹配包含'. '和'*'的正则表达式。模式中的字符'.'表示任意一个字符，
+ * 而'*'表示它前面的字符可以出现任意次（含0次）。在本题中，匹配是指字符串的所有字符匹配整个模式。
+ * 例如，字符串"aaa"与模式"a.a"和"ab*ac*a"匹配，但与"aa.a"和"ab*a"均不匹配
+ * Author:   @VirtualChen
+ * Date:     2020/3/23 20:57
+ */
+package src;
+
+public class Regularexpressionmatching {
+    public boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;//dp[i][j] 表示 s 的前 i 个是否能被 p 的前 j 个匹配
+        for (int i = 0; i < p.length(); i++) { //P的长度
+                if (p.charAt(i) == '*' && dp[0][i - 1]) {
+                    dp[0][i + 1] = true;
+                }
+        }
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (p.charAt(j) == '.' || p.charAt(j) == s.charAt(i)) {//如果是任意元素 或者是对于元素匹配
+                    dp[i + 1][j + 1] = dp[i][j];
+                }
+                if (p.charAt(j) == '*') {
+                    if (p.charAt(j - 1) != s.charAt(i) && p.charAt(j - 1) != '.') {//如果前一个元素不匹配 且不为任意元素
+                        dp[i + 1][j + 1] = dp[i + 1][j - 1];
+                    } else {
+                        dp[i + 1][j + 1] = (dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1]);
+                            /*
+                            dp[i][j] = dp[i-1][j] // 多个字符匹配的情况
+                            or dp[i][j] = dp[i][j-1] // 单个字符匹配的情况
+                            or dp[i][j] = dp[i][j-2] // 没有匹配的情况
+                             */
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
+}
